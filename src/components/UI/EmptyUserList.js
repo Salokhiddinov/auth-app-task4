a1596c0bc40e618876ddc717c8a9c6d15e32a93f
Nomonjon <74725160+Salokhiddinov@users.noreply.github.com>
2022-08-03 14:45:12 +0500

    fix: fix EmptyUserList.js component

--- src/components/UI/EmptyUserList.js
+++ src/components/UI/EmptyUserList.js
@@ -3,9 +3,10 @@ import { Link } from "react-router-dom";
 function EmptyUserList() {
   return (
     <form>
-      <h2>Ooops! No access to user's list</h2>
-      <p>To access list, please log in</p>
+      <h2>Hey! You don't have access to user's list ☹️</h2>
+      <p>To access list, please log in or sign up</p>
         <Link to="/log-in" className="btn btn-secondary">Log in</Link>
+        <Link to="/sign-up" className="btn btn-success">Sign up</Link>
     </form>
   );
 }
